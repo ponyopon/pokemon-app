eefe67a807d97764bdf7fa82c9955086c47b1d5e
Shoji Eiki <eiki.s50311998@gmail.com>
2023-09-25 11:17:11 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import { getAllPokemon } from './utils/pokemon';
+import { getPokemon } from './utils/pokemon';
+import Card from './components/Card/Card';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState('');
+
+  useEffect( () => {
+    const fetchPokemonData = async () => {
+      //すべてのデータを取得
+      let res = await getAllPokemon(initialURL);
+      //各ポケモンの詳細のデータを取得
+      loadPokemon(res.results);
+      setNextUrl(res.next);
+      setLoading(false);
+      
+    };
+    fetchPokemonData();
+})
+
+const loadPokemon = async (data) => {
+  //全て待つよ
+  let _pokemonData = await Promise.all(
+    data.map((pokemon) => {
+      let pokemonRecord = getPokemon(pokemon.url);
+      return pokemonRecord;
+    })
+  );
+  setPokemonData(_pokemonData);
+};
+const handleNextPage = async() => {
+  setLoading(true);
+  let data = await getAllPokemon(nextUrl)
+};
+const handlePrevPage = () => {};
+
+  return (
+    <>
+    <Navbar />
+  <div className='App'>
+  {loading ? (
+    <h1>ロード中</h1>
+  ): (
+  <>
+    <div className='pokemonCardContainer'>
+      {pokemonData.map((pokemon, i) => {
+        return <Card key={i} pokemon={pokemon}>Pokemon</Card>
+      })}
+    </div>
+    <div className='btn'>
+      <button onClick={handlePrevPage}>前へ</button>
+      <button onClick={handleNextPage}>次へ</button>
+    </div>
+
+  </>
+  )}
+  </div>
+  </>
+  );
+
+}
+
+
+export default App;
